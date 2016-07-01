f9729d1f9d05eb31a2fc3d401709b0ed1fe9d269
Emilio Pinna <emilio.pinn@gmail.com>
2016-07-01 14:43:59 +0100

    Add PHP web responder

---
+++ tests/test_php_smarty.py
@@ -0,0 +1,13 @@
+import unittest
+import requests
+import os
+import sys
+import random
+
+sys.path.insert(1, os.path.join(sys.path[0], '..'))
+from plugins.engines.smarty import Smarty
+from core.http import Channel
+
+class SmartyTest(unittest.TestCase):
+    
+    pass
--- tests/tests.sh
+++ tests/tests.sh
@@ -12,9 +12,16 @@ curl http://localhost:15001/shutdown
 # Run PHP webserver
 php -S localhost:8000 -t _test_php_smarty/&
 PHPPID=$!
+# Download smarty if not already installed
+if [ ! -d ./_test_php_smarty/smarty/ ]; then
+    wget https://github.com/smarty-php/smarty/archive/v3.1.29.tar.gz -O ./_test_php_smarty/v3.1.29.tar.gz
+    tar xvf ./_test_php_smarty/v3.1.29.tar.gz -C ./_test_php_smarty/
+    mv ./_test_php_smarty/smarty-3.1.29/libs ./_test_php_smarty/smarty/
+    rm -r ./_test_php_smarty/smarty-3.1.29/ ./_test_php_smarty/v3.1.29.tar.gz
+fi
 sleep 1
 # Launch PHP engines tests
-python -m unittest discover . 'test_hp_*.py'
+python -m unittest discover . 'test_php_*.py'
 sleep 1
 # Shutdown PHP webserver 
 kill ${PHPPID}
