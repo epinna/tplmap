a42e84b62eb3cd813976409a0e97db22dfaa45bc
Emilio Pinna <emilio.pinn@gmail.com>
2016-07-01 13:46:41 +0100

    Add Smarty testsuite base files

--- tests/tests.sh
+++ tests/tests.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
-# Start python webserver
+# Run python webserver
 python webserver.py&
-
 sleep 1
-
-python -m unittest discover . 'test_*.py'
-
-
+# Launch python engines tests
+python -m unittest discover . 'test_py_*.py'
 sleep 1
 # Shutdown python webserver 
 curl http://localhost:15001/shutdown
 
+# Run PHP webserver
+php -S localhost:8000 -t _test_php_smarty/&
+PHPPID=$!
+sleep 1
+# Launch PHP engines tests
+python -m unittest discover . 'test_hp_*.py'
+sleep 1
+# Shutdown PHP webserver 
+kill ${PHPPID}
