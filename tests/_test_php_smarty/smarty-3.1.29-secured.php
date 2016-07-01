c0b9c5d0c51b8506cafe36ad16bfe6acb68d9d81
Emilio Pinna <emilio.pinn@gmail.com>
2016-07-01 18:35:27 +0100

    Simplify smarty PHP tests

--- tests/_test_php_smarty/smarty-3.1.29-secured.php
+++ tests/_test_php_smarty/smarty-3.1.29-secured.php
@@ -1,11 +1,8 @@
 <?php
 require('smarty-3.1.29/libs/Smarty.class.php');
 $smarty = new Smarty;
-if(!array_key_exists('tpl', $_GET)) {
-  $tpl="${_GET['inj']}";
-}
-else {
-  $tpl=$_GET['inj'];
-}
+
+$tpl=$_GET['inj'];
+
 echo($smarty->fetch('string:'.$tpl));
 ?>
--- tests/_test_php_smarty/smarty-3.1.29-unsecured.php
+++ tests/_test_php_smarty/smarty-3.1.29-unsecured.php
@@ -1,11 +1,7 @@
 <?php
 require('smarty-3.1.29/libs/Smarty.class.php');
 $smarty = new SmartyBC;
-if(!array_key_exists('tpl', $_GET)) {
-  $tpl="${_GET['inj']}";
-}
-else {
-  $tpl=$_GET['inj'];
-}
+
+$tpl=$_GET['inj'];
 echo($smarty->fetch('string:'.$tpl));
 ?>
