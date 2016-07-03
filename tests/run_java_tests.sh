1f3cf722d355a3666498c9ad172c3f2428f6f99b
Emilio Pinna <emilio.pinn@gmail.com>
2016-07-03 21:51:03 +0100

    Complete run_java_tests script

--- tests/run_java_tests.sh
+++ tests/run_java_tests.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 gradle -v >/dev/null 2>&1 || { echo >&2 "Gradle required but it's not installed.  Aborting."; exit 1; }
+type nc >/dev/null 2>&1 || { echo >&2 "Netcat required but it's not installed.  Aborting."; exit 1; }
 
 #if [ ! -d ./_test_java_freemarker/lib/spark-example/ ]; then
 rm  -rf ./_test_java_freemarker/lib/spark-app/
@@ -8,4 +9,12 @@ cp -rf ./_test_java_freemarker/spark-app ./_test_java_freemarker/lib/spark-app/
 #fi
 
 cd ./_test_java_freemarker/lib/spark-app/
-gradle run --debug
+gradle run --debug&
+GRADLEPID=$!
+
+while ! echo exit | nc localhost 15001; do sleep 10; done
+
+python -m unittest discover . 'test_java_*.py'
+sleep 1
+# Shutdown Java webserver 
+kill ${GRADLEPID}
