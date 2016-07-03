febc24fe908cefb861b147ca27bce56850eed6dc
Emilio Pinna <emilio.pinn@gmail.com>
2016-07-03 19:43:26 +0100

    Add running base Spark Java webserver

--- tests/run_java_tests.sh
+++ tests/run_java_tests.sh
@@ -2,9 +2,10 @@
 
 gradle -v >/dev/null 2>&1 || { echo >&2 "Gradle required but it's not installed.  Aborting."; exit 1; }
 
-if [ ! -d ./_test_java_freemarker/lib/spark-example/ ]; then
-  cp -rf ./_test_java_freemarker/spark-example ./_test_java_freemarker/lib/spark-example/
-fi
+#if [ ! -d ./_test_java_freemarker/lib/spark-example/ ]; then
+rm  -rf ./_test_java_freemarker/lib/spark-app/
+cp -rf ./_test_java_freemarker/spark-app ./_test_java_freemarker/lib/spark-app/
+#fi
 
-cd ./_test_java_freemarker/lib/spark-example/
-gradle run
+cd ./_test_java_freemarker/lib/spark-app/
+gradle run --stacktrace
