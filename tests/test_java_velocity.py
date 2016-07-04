82980469f36c8a4fabfd6868c5f76383fb85fe49
Emilio Pinna <emilio.pinn@gmail.com>
2016-07-04 14:15:47 +0100

    Add Velocity test script

---
+++ tests/test_java_velocity.py
@@ -0,0 +1,36 @@
+import unittest
+import requests
+import os
+import sys
+import random
+
+sys.path.insert(1, os.path.join(sys.path[0], '..'))
+from plugins.engines.velocity import Velocity
+from core.channel import Channel
+
+class VelocityTest(unittest.TestCase):
+    
+    def test_reflection(self):
+        
+        template = '%s'
+        
+        channel = Channel('http://127.0.0.1:15001/velocity?inj=*')
+        Velocity(channel)
+        self.assertEqual(channel.data, {
+            'reflect_tag': '#set($p=%s)\n$p',
+            'language': 'java',
+            'engine': 'velocity'
+        })
+
+    
+    def test_reflection_within_text(self):
+        template = 'AAAA%sAAAA'
+        
+        channel = Channel('http://127.0.0.1:15001/velocity?inj=*')
+        Velocity(channel)
+        self.assertEqual(channel.data, {
+            'reflect_tag': '#set($p=%s)\n$p',
+            'language': 'java',
+            'engine': 'velocity'
+        })
+        
