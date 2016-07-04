eca606d49953718d8a6431e0818d5dcc8e310c7d
Emilio Pinna <emilio.pinn@gmail.com>
2016-07-04 18:57:24 +0100

    Add infrastructure for node jade tests

---
+++ tests/_test_node_jade/connect-app.js
@@ -0,0 +1,20 @@
+var connect = require('connect');
+var http = require('http');
+var url = require('url');
+var jade = require('jade');
+ 
+var app = connect();
+ 
+// respond to all requests 
+app.use('/jade', function(req, res){
+  if(req.url) {
+    var url_parts = url.parse(req.url, true);
+    if('inj' in url_parts.query) {
+      res.end(jade.render(url_parts.query.inj));  
+    }
+    res.end();
+  }
+});
+ 
+//create node.js http server and listen on port 
+http.createServer(app).listen(15001);
---
+++ tests/run_node_tests.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+npm -v >/dev/null 2>&1 || { echo >&2 "NPM required but it's not installed.  Aborting."; exit 1; }
+
+cd _test_node_jade/lib/
+npm install connect
+
+cp ../connect-app.js connect-app.js 
+
+node connect-app.js &
+NODEPID=$!
+
+#while ! echo exit | nc localhost 15001; do sleep 10; done
+
+cd ../../
+python -m unittest discover . 'test_node_*.py'
+sleep 1
+# Shutdown Java webserver 
+kill ${NODEPID}
