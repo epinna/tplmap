0765fbcda24c5133e4a2be830c0b7a0d568a5094
Emilio Pinna <emilio.pinn@gmail.com>
2016-07-04 17:33:42 +0100

    Rename test channel

---
+++ tests/test_channel_fields.py
@@ -0,0 +1,45 @@
+import unittest
+import requests
+import os
+import sys
+import random
+
+sys.path.insert(1, os.path.join(sys.path[0], '..'))
+from plugins.engines.mako import Mako
+from core.channel import Channel
+
+class ChannelTest(unittest.TestCase):
+    
+    def test_post_reflection(self):
+        
+        template = '%s'
+        
+        channel = Channel({
+            'url' : 'http://127.0.0.1:15001/post/mako',
+            'post_data' : [ 'inj=*' ]
+        })
+        Mako(channel)
+        self.assertEqual(channel.data, { 
+            'reflect_tag': '${%s}',
+            'language': 'python',
+            'engine': 'mako',  
+            'exec' : True,
+            'os' : 'posix-darwin'
+        })
+
+    def test_header_reflection(self):
+        
+        template = '%s'
+        
+        channel = Channel({
+            'url' : 'http://127.0.0.1:15001/header/mako',
+            'headers' : [ 'User-Agent: *' ]
+        })
+        Mako(channel)
+        self.assertEqual(channel.data, { 
+            'reflect_tag': '${%s}',
+            'language': 'python',
+            'engine': 'mako',  
+            'exec' : True,
+            'os' : 'posix-darwin'
+        })
