186435e868330b8f12919d9bcbce19dd487eeb94
Emilio Pinna <emilio.pinn@gmail.com>
2016-07-04 11:15:32 +0100

    Add GET parameter handlig in Channel

--- core/channel.py
+++ core/channel.py
@@ -1,6 +1,6 @@
 import requests
 from utils.loggers import log
-
+import urlparse
 
 class Channel:
     
@@ -9,9 +9,33 @@ class Channel:
         self.url = url
         self.data = {}
         
+        self.get_params = {}
+        self.get_placeholders = []
+        
         if '*' not in self.url:
             log.warn("no GET or parameter(s) found for testing ")
+
+        self._parse_get()
+
+    def _parse_get(self):
         
+        params_dict_list = urlparse.parse_qs(urlparse.urlsplit(self.url).query)
+        
+        for param, value_list in params_dict_list.items():
+            self.get_params[param] = value_list
+            
+            if any(x for x in value_list if '*' in x):
+                self.get_placeholders.append(param)
+                log.warn('Found placeholder in parameter \'%s\'' % param)
+                
+            
     def req(self, injection):
-        return requests.get(self.url.replace('*', injection)).text
+        
+        # Get base URL
+        url_string = self.url.split("?")[0]
+        get_placeholder = self.get_placeholders[0]
+            
+        get_params = self.get_params.copy()
+        get_params[get_placeholder] = injection
+        return requests.get(url_string, params = get_params).text
         
