94e8ba2b1140fa8fd75e742914e00be6ebdccdbb
Emilio Pinna <emilio.pinna@barclays.com>
2016-07-06 16:28:26 +0100

    Adjust channel tests

--- tests/run_channel_test.sh
+++ tests/run_channel_test.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+cd env_py_tests/
 # Run python webserver
 python webserver.py&
+cd ..
 sleep 1
 # Launch python engines tests
 python -m unittest discover . 'test_channel_*.py'
 sleep 1
-# Shutdown python webserver 
+# Shutdown python webserver
 curl http://localhost:15001/shutdown
--- tests/test_channel_fields.py
+++ tests/test_channel_fields.py
@@ -9,55 +9,48 @@ from plugins.engines.mako import Mako
 from core.channel import Channel
 
 class ChannelTest(unittest.TestCase):
-    
+
+    expected_data = {
+        'language': 'python',
+        'engine': 'mako',
+        'eval' : 'python' ,
+        'exec' : True,
+        'os' : 'posix-darwin',
+        'trailer_tag': '${%(trailer)s}',
+        'header_tag': '${%(header)s}',
+        'render_tag': '${%(payload)s}',
+    }
+
     def test_post_reflection(self):
-        
+
         template = '%s'
-        
+
         channel = Channel({
             'url' : 'http://127.0.0.1:15001/post/mako',
             'post_data' : [ 'inj=*' ]
         })
         Mako(channel).detect()
-        self.assertEqual(channel.data, { 
-            'reflect_tag': '${%s}',
-            'language': 'python',
-            'engine': 'mako',  
-            'exec' : True,
-            'os' : 'posix-darwin'
-        })
+        self.assertEqual(channel.data, self.expected_data)
 
     def test_header_reflection(self):
-        
+
         template = '%s'
-        
+
         channel = Channel({
             'url' : 'http://127.0.0.1:15001/header/mako',
             'headers' : [ 'User-Agent: *' ]
         })
         Mako(channel).detect()
-        self.assertEqual(channel.data, { 
-            'reflect_tag': '${%s}',
-            'language': 'python',
-            'engine': 'mako',  
-            'exec' : True,
-            'os' : 'posix-darwin'
-        })
+        self.assertEqual(channel.data, self.expected_data)
 
     def test_put_reflection(self):
-        
+
         template = '%s'
-        
+
         channel = Channel({
             'url' : 'http://127.0.0.1:15001/put/mako',
             'post_data' : [ 'inj=*' ],
             'method' : 'PUT'
         })
         Mako(channel).detect()
-        self.assertEqual(channel.data, { 
-            'reflect_tag': '${%s}',
-            'language': 'python',
-            'engine': 'mako',  
-            'exec' : True,
-            'os' : 'posix-darwin'
-        })
+        self.assertEqual(channel.data, self.expected_data)
