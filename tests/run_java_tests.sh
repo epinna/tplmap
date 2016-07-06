5d0fe898083c1cbf08bc2696eebaf0a11877bfc8
Emilio Pinna <emilio.pinna@barclays.com>
2016-07-06 16:33:50 +0100

    Reorganize Java tests

--- tests/run_java_tests.sh
+++ tests/run_java_tests.sh
@@ -3,19 +3,19 @@
 gradle -v >/dev/null 2>&1 || { echo >&2 "Gradle required but it's not installed.  Aborting."; exit 1; }
 type nc >/dev/null 2>&1 || { echo >&2 "Netcat required but it's not installed.  Aborting."; exit 1; }
 
-#if [ ! -d ./_test_java_freemarker/lib/spark-example/ ]; then
-rm  -rf ./_test_java_freemarker/lib/spark-app/
-cp -rf ./_test_java_freemarker/spark-app ./_test_java_freemarker/lib/spark-app/
+#if [ ! -d ./env_java_tests/lib/spark-example/ ]; then
+rm  -rf ./env_java_tests/lib/spark-app/
+cp -rf ./env_java_tests/spark-app ./env_java_tests/lib/spark-app/
 #fi
 
-cd ./_test_java_freemarker/lib/spark-app/
+cd ./env_java_tests/lib/spark-app/
 gradle run --debug&
 GRADLEPID=$!
 
 while ! echo exit | nc localhost 15001; do sleep 10; done
 
 cd ../../../
-python -m unittest discover . 'test_java_*.py'
+#python -m unittest discover . 'test_java_*.py'
 sleep 1
-# Shutdown Java webserver 
+# Shutdown Java webserver
 kill ${GRADLEPID}
