643f7a05761b7d39b3f0fd507efbc4445b138027
Emilio Pinna <emilio.pinna@barclays.com>
2016-07-06 15:52:17 +0100

    Now randint_n returns only numbers > 2 to avoid *1 multiplication which creates false positive

--- utils/rand.py
+++ utils/rand.py
@@ -2,10 +2,19 @@ import random
 import string
 
 def randint_n(n):
-    range_start = 10**(n-1)
+
+    # If the length is 1, starts from 2 to avoid
+    # number repetition on evaluation e.g. 1*8=8
+    # creating false positives
+
+    if n == 1:
+        range_start = 2
+    else:
+        range_start = 10**(n-1)
+
     range_end = (10**n)-1
     return random.randint(range_start, range_end)
-    
+
 def randstr_n(n):
     return ''.join(
         random.choice(string.letters + string.digits) for _ in range(n)
