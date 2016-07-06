c0fa4cef9d7e25c27fa8ab76aff412987d24a44c
Emilio Pinna <emilio.pinna@barclays.com>
2016-07-06 16:23:02 +0100

    Refactor PHP test folder name

--- tests/run_php_tests.sh
+++ tests/run_php_tests.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # Run PHP webserver
-php -S 127.0.0.1:15002 -t test_php_env/&
+php -S 127.0.0.1:15002 -t env_php_tests/&
 PHPPID=$!
 # Download smarty 3.1.29 if not already installed
-if [ ! -d ./test_php_env/lib/smarty-3.1.29/ ]; then
-    wget https://github.com/smarty-php/smarty/archive/v3.1.29.tar.gz -O ./test_php_env/lib/v3.1.29.tar.gz
-    tar xvf ./test_php_env/lib/v3.1.29.tar.gz -C ./test_php_env/lib/
-    rm ./test_php_env/lib/v3.1.29.tar.gz
+if [ ! -d ./env_php_tests/lib/smarty-3.1.29/ ]; then
+    wget https://github.com/smarty-php/smarty/archive/v3.1.29.tar.gz -O ./env_php_tests/lib/v3.1.29.tar.gz
+    tar xvf ./env_php_tests/lib/v3.1.29.tar.gz -C ./env_php_tests/lib/
+    rm ./env_php_tests/lib/v3.1.29.tar.gz
 fi
 # Download twig 1.24.1 if not already installed
-if [ ! -d ./test_php_env/lib/twig-1.24.1/ ]; then
-    wget https://github.com/twigphp/Twig/archive/v1.24.1.tar.gz -O ./test_php_env/lib/v1.24.1.tar.gz
-    tar xvf ./test_php_env/lib/v1.24.1.tar.gz -C ./test_php_env/lib/
-    rm ./test_php_env/lib/v1.24.1.tar.gz
+if [ ! -d ./env_php_tests/lib/twig-1.24.1/ ]; then
+    wget https://github.com/twigphp/Twig/archive/v1.24.1.tar.gz -O ./env_php_tests/lib/v1.24.1.tar.gz
+    tar xvf ./env_php_tests/lib/v1.24.1.tar.gz -C ./env_php_tests/lib/
+    rm ./env_php_tests/lib/v1.24.1.tar.gz
 fi
 sleep 1
 # Launch PHP engines tests
