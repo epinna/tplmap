55e3a2a612919fbfa00afe709e61cb41c8b8cec3
Emilio Pinna <emilio.pinn@gmail.com>
2016-07-10 00:02:45 +0100

    Add Jinja2 --write-file and --read-file tests

--- tests/test_py_jinja2.py
+++ tests/test_py_jinja2.py
@@ -7,6 +7,8 @@ import random
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
 from plugins.engines.jinja2 import Jinja2
 from core.channel import Channel
+from utils import rand
+from utils import strings
 
 class Jinja2Test(unittest.TestCase):
 
@@ -15,6 +17,8 @@ class Jinja2Test(unittest.TestCase):
         'engine': 'jinja2',
         'eval' : 'python' ,
         'exec' : True,
+        'read' : True,
+        'write' : True,
         'trailer_tag': '{{%(trailer)s}}',
         'header_tag': '{{%(header)s}}',
         'render_tag': '{{%(payload)s}}',
@@ -80,3 +84,67 @@ class Jinja2Test(unittest.TestCase):
         
         result = jinja2.execute('echo 1\\"2')
         self.assertEqual(result, '1"2')
+        
+        
+    def test_file_read(self):
+        template = 'AAAA%sAAAA'
+
+        channel = Channel({
+            'url' : 'http://127.0.0.1:15001/reflect/jinja2?inj=*'
+        })
+        jinja2obj = Jinja2(channel)
+        jinja2obj.detect()
+        del channel.data['os']
+        self.assertEqual(channel.data, self.expected_data)
+        
+        # Normal ASCII file
+        readable_file = '/etc/resolv.conf'
+        content = open(readable_file, 'r').read()
+        self.assertEqual(content, jinja2obj.read(readable_file))
+        
+        # Long binary file
+        readable_file = '/bin/ls'
+        content = open(readable_file, 'rb').read()
+        self.assertEqual(content, jinja2obj.read(readable_file))    
+        
+        # Non existant file
+        self.assertEqual(None, jinja2obj.read('/nonexistant'))
+        # Unpermitted file
+        self.assertEqual(None, jinja2obj.read('/etc/shadow'))
+        # Empty file
+        self.assertEqual('', jinja2obj.read('/dev/null'))
+
+    def test_file_write(self):
+        template = 'AAAA%sAAAA'
+
+        channel = Channel({
+            'url' : 'http://127.0.0.1:15001/reflect/jinja2?inj=*'
+        })
+        jinja2obj = Jinja2(channel)
+        jinja2obj.detect()
+        del channel.data['os']
+        self.assertEqual(channel.data, self.expected_data)
+        
+        remote_temp_path = '/tmp/tplmap_%s.tmp' % rand.randstr_n(10)
+        
+        # Send long binary
+        data = open('/bin/ls', 'rb').read()
+        jinja2obj.write(data, remote_temp_path)
+        self.assertEqual(jinja2obj._md5(remote_temp_path), strings.md5(data))
+        jinja2obj.execute('rm %s' % (remote_temp_path))
+        
+        # Send short ASCII data, without removing it
+        data = 'SHORT ASCII DATA'
+        jinja2obj.write(data, remote_temp_path)
+        self.assertEqual(jinja2obj._md5(remote_temp_path), strings.md5(data))
+
+        # Try to append data without --force-overwrite and re-check the previous md5
+        jinja2obj.write('APPENDED DATA', remote_temp_path)
+        self.assertEqual(jinja2obj._md5(remote_temp_path), strings.md5(data))
+        
+        # Now set --force-overwrite and rewrite new data on the same file
+        jinja2obj.channel.args['force_overwrite'] = True
+        data = 'NEW DATA'
+        jinja2obj.write(data, remote_temp_path)
+        self.assertEqual(jinja2obj._md5(remote_temp_path), strings.md5(data))
+        jinja2obj.execute('rm %s' % (remote_temp_path))
