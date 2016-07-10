e07e9b806719a7e490518613b5a1d37d0ca8df49
Emilio Pinna <emilio.pinn@gmail.com>
2016-07-10 14:40:32 +0100

    Use BaseTest as base class for Mako and Jinja2 tests

--- tests/test_py_jinja2.py
+++ tests/test_py_jinja2.py
@@ -9,8 +9,9 @@ from plugins.engines.jinja2 import Jinja2
 from core.channel import Channel
 from utils import rand
 from utils import strings
+from basetest import BaseTest
 
-class Jinja2Test(unittest.TestCase):
+class Jinja2Test(unittest.TestCase, BaseTest):
 
     expected_data = {
         'language': 'python',
@@ -23,128 +24,31 @@ class Jinja2Test(unittest.TestCase):
         'header_tag': '{{%(header)s}}',
         'render_tag': '{{%(payload)s}}',
     }
-
-    def test_reflection(self):
-
-        template = '%s'
-
-        channel = Channel({
-            'url' : 'http://127.0.0.1:15001/reflect/jinja2?tpl=%s&inj=*' % template
-        })
-        Jinja2(channel).detect()
-        del channel.data['os']
-        self.assertEqual(channel.data, self.expected_data)
-
-
-    def test_reflection_context_text(self):
-        template = 'AAAA%sAAAA'
-
-        channel = Channel({
-            'url' : 'http://127.0.0.1:15001/reflect/jinja2?tpl=%s&inj=*' % template
-        })
-        Jinja2(channel).detect()
-        del channel.data['os']
-        self.assertEqual(channel.data, self.expected_data)
-
-    def test_reflection_context_code(self):
-        template = '{{%s}}'
-
-        channel = Channel({
-            'url' : 'http://127.0.0.1:15001/reflect/jinja2?tpl=%s&inj=*' % template
-        })
-        Jinja2(channel).detect()
-
-        expected_data = self.expected_data.copy()
-        expected_data.update({ 'prefix' : '""}}', 'suffix' : '{{""' })
-
-        del channel.data['os']
-        self.assertEqual(channel.data, expected_data)
+    
+    url = 'http://127.0.0.1:15001/reflect/jinja2?tpl=%s&inj=*'
+    plugin = Jinja2
+    
+    reflection_tests = [
+        ('%s', {}),
+        ('AAA%sAAA', {}),
+        ('{{%s}}', { 'prefix' : '""}}', 'suffix' : '{{""' })
+    ]
 
     def test_reflection_limit(self):
-        template = '%s'
-
-        channel = Channel({
-            'url' : 'http://127.0.0.1:15001/limit/jinja2?tpl=%s&inj=*&limit=8' % template
-        })
-
-        Jinja2(channel).detect()
+        
+        obj, data = self._get_detection_obj_data('http://127.0.0.1:15001/limit/jinja2?tpl=%s&inj=*&limit=8' % '')
 
         expected_data = { 'render_tag' : self.expected_data['render_tag'] }
         
-        self.assertEqual(channel.data, expected_data)
+        self.assertEqual(data, expected_data)
 
     def test_reflection_quotes(self):
-        channel = Channel({
-            'url' : 'http://127.0.0.1:15001/reflect/jinja2?&inj=*',
-        })
+        
+        obj, data = self._get_detection_obj_data(self.url % '')
 
-        jinja2 = Jinja2(channel)
-        result = jinja2.execute('echo 1"2"')
+        result = obj.execute('echo 1"2"')
         self.assertEqual(result, '12')
         
-        result = jinja2.execute('echo 1\\"2')
+        result = obj.execute('echo 1\\"2')
         self.assertEqual(result, '1"2')
         
-        
-    def test_download(self):
-        template = 'AAAA%sAAAA'
-
-        channel = Channel({
-            'url' : 'http://127.0.0.1:15001/reflect/jinja2?inj=*'
-        })
-        jinja2obj = Jinja2(channel)
-        jinja2obj.detect()
-        del channel.data['os']
-        self.assertEqual(channel.data, self.expected_data)
-        
-        # Normal ASCII file
-        readable_file = '/etc/resolv.conf'
-        content = open(readable_file, 'r').read()
-        self.assertEqual(content, jinja2obj.read(readable_file))
-        
-        # Long binary file
-        readable_file = '/bin/ls'
-        content = open(readable_file, 'rb').read()
-        self.assertEqual(content, jinja2obj.read(readable_file))    
-        
-        # Non existant file
-        self.assertEqual(None, jinja2obj.read('/nonexistant'))
-        # Unpermitted file
-        self.assertEqual(None, jinja2obj.read('/etc/shadow'))
-        # Empty file
-        self.assertEqual('', jinja2obj.read('/dev/null'))
-
-    def test_upload(self):
-        template = 'AAAA%sAAAA'
-
-        channel = Channel({
-            'url' : 'http://127.0.0.1:15001/reflect/jinja2?inj=*'
-        })
-        jinja2obj = Jinja2(channel)
-        jinja2obj.detect()
-        del channel.data['os']
-        self.assertEqual(channel.data, self.expected_data)
-        
-        remote_temp_path = '/tmp/tplmap_%s.tmp' % rand.randstr_n(10)
-        
-        # Send long binary
-        data = open('/bin/ls', 'rb').read()
-        jinja2obj.write(data, remote_temp_path)
-        self.assertEqual(jinja2obj._md5(remote_temp_path), strings.md5(data))
-        jinja2obj.execute('rm %s' % (remote_temp_path))
-        
-        # Send short ASCII data, without removing it
-        data = 'SHORT ASCII DATA'
-        jinja2obj.write(data, remote_temp_path)
-        self.assertEqual(jinja2obj._md5(remote_temp_path), strings.md5(data))
-
-        # Try to append data without --force-overwrite and re-check the previous md5
-        jinja2obj.write('APPENDED DATA', remote_temp_path)
-        self.assertEqual(jinja2obj._md5(remote_temp_path), strings.md5(data))
-        
-        # Now set --force-overwrite and rewrite new data on the same file
-        jinja2obj.channel.args['force_overwrite'] = True
-        data = 'NEW DATA'
-        jinja2obj.write(data, remote_temp_path)
-        self.assertEqual(jinja2obj._md5(remote_temp_path), strings.md5(data))
-        jinja2obj.execute('rm %s' % (remote_temp_path))
--- tests/test_py_mako.py
+++ tests/test_py_mako.py
@@ -9,8 +9,9 @@ from plugins.engines.mako import Mako
 from core.channel import Channel
 from utils import rand
 from utils import strings
+from basetest import BaseTest
 
-class MakoTest(unittest.TestCase):
+class MakoTest(unittest.TestCase, BaseTest):
 
     expected_data = {
         'language': 'python',
@@ -23,48 +24,21 @@ class MakoTest(unittest.TestCase):
         'header_tag': '${%(header)s}',
         'render_tag': '${%(payload)s}',
     }
-
-    def test_reflection(self):
-
-        template = '%s'
-
-        channel = Channel({
-            'url' : 'http://127.0.0.1:15001/reflect/mako?tpl=%s&inj=*' % template
-        })
-        Mako(channel).detect()
-        del channel.data['os']
-        self.assertEqual(channel.data, self.expected_data)
-
-
-    def test_reflection_context_text(self):
-        template = 'AAAA%sAAAA'
-
-        channel = Channel({
-            'url' : 'http://127.0.0.1:15001/reflect/mako?tpl=%s&inj=*' % template
-        })
-        Mako(channel).detect()
-        del channel.data['os']
-        self.assertEqual(channel.data, self.expected_data)
-
-    def test_reflection_context_code(self):
-        template = '${%s}'
-
-        channel = Channel({
-            'url' : 'http://127.0.0.1:15001/reflect/mako?tpl=%s&inj=*' % template
-        })
-        Mako(channel).detect()
-
-        expected_data = self.expected_data.copy()
-        expected_data.update({ 'prefix' : '}', 'suffix' : '${' })
+    
+    url = 'http://127.0.0.1:15001/reflect/mako?tpl=%s&inj=*'
+    plugin = Mako
+    
+    reflection_tests = [
+        ('%s', {}),
+        ('AAA%sAAA', {}),
+        ('${%s}', { 'prefix' : '}', 'suffix' : '${' })
+    ]
         
-        del channel.data['os']
-        self.assertEqual(channel.data, expected_data)
-
     def test_reflection_limit(self):
         template = '%s'
 
         channel = Channel({
-            'url' : 'http://127.0.0.1:15001/limit/mako?tpl=%s&inj=*' % template
+            'url' : 'http://127.0.0.1:15001/limit/mako?tpl=%s&inj=*&limit=6' % template
         })
 
         Mako(channel).detect()
@@ -72,65 +46,3 @@ class MakoTest(unittest.TestCase):
         expected_data = { 'render_tag' : self.expected_data['render_tag'] }
         
         self.assertEqual(channel.data, expected_data)
-        
-        
-    def test_download(self):
-
-        channel = Channel({
-            'url' : 'http://127.0.0.1:15001/reflect/mako?inj=*'
-        })
-        makoobj = Mako(channel)
-        makoobj.detect()
-        del channel.data['os']
-        self.assertEqual(channel.data, self.expected_data)
-        
-        # Normal ASCII file
-        readable_file = '/etc/resolv.conf'
-        content = open(readable_file, 'r').read()
-        self.assertEqual(content, makoobj.read(readable_file))
-        
-        # Long binary file
-        readable_file = '/bin/ls'
-        content = open(readable_file, 'rb').read()
-        self.assertEqual(content, makoobj.read(readable_file))    
-        
-        # Non existant file
-        self.assertEqual(None, makoobj.read('/nonexistant'))
-        # Unpermitted file
-        self.assertEqual(None, makoobj.read('/etc/shadow'))
-        # Empty file
-        self.assertEqual('', makoobj.read('/dev/null'))
-
-    def test_upload(self):
-
-        channel = Channel({
-            'url' : 'http://127.0.0.1:15001/reflect/mako?inj=*'
-        })
-        makoobj = Mako(channel)
-        makoobj.detect()
-        del channel.data['os']
-        self.assertEqual(channel.data, self.expected_data)
-        
-        remote_temp_path = '/tmp/tplmap_%s.tmp' % rand.randstr_n(10)
-        
-        # Send long binary
-        data = open('/bin/ls', 'rb').read()
-        makoobj.write(data, remote_temp_path)
-        self.assertEqual(makoobj._md5(remote_temp_path), strings.md5(data))
-        makoobj.execute('rm %s' % (remote_temp_path))
-        
-        # Send short ASCII data, without removing it
-        data = 'SHORT ASCII DATA'
-        makoobj.write(data, remote_temp_path)
-        self.assertEqual(makoobj._md5(remote_temp_path), strings.md5(data))
-
-        # Try to append data without --force-overwrite and re-check the previous md5
-        makoobj.write('APPENDED DATA', remote_temp_path)
-        self.assertEqual(makoobj._md5(remote_temp_path), strings.md5(data))
-        
-        # Now set --force-overwrite and rewrite new data on the same file
-        makoobj.channel.args['force_overwrite'] = True
-        data = 'NEW DATA'
-        makoobj.write(data, remote_temp_path)
-        self.assertEqual(makoobj._md5(remote_temp_path), strings.md5(data))
-        makoobj.execute('rm %s' % (remote_temp_path))
