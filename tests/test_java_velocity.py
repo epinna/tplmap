b6ea3d07f32d560d674ab5770f142e4b595590d7
Emilio Pinna <emilio.pinna@barclays.com>
2016-07-11 12:49:22 +0100

    Update Velocity tests with the new payloads

--- tests/test_java_velocity.py
+++ tests/test_java_velocity.py
@@ -9,19 +9,19 @@ from plugins.engines.velocity import Velocity
 from basetest import BaseTest
 
 class VelocityTest(unittest.TestCase, BaseTest):
-    
+
     expected_data = {
         'language': 'java',
         'engine': 'velocity',
-        'trailer_tag': '\n#set($t=%(trailer)s)\n$t',
-        'header_tag': '#set($h=%(header)s)\n$h\n',
-        'render_tag': '#set($p=%(payload)s)\n$p\n',
+        'trailer_tag': '\n#set($t=%(trailer)s)\n${t}',
+        'header_tag': '#set($h=%(header)s)\n${h}\n',
+        'render_tag': '#set($p=%(payload)s)\n${p}\n',
     }
-    
+
     url = 'http://127.0.0.1:15003/velocity?inj=*&tpl=%s'
 
     plugin = Velocity
-    
+
     reflection_tests = [
         (1, '%s', {}),
         (1, 'AAA%sAAA', {})
@@ -29,6 +29,6 @@ class VelocityTest(unittest.TestCase, BaseTest):
 
     def test_download(self):
         pass
-        
+
     def test_upload(self):
         pass
