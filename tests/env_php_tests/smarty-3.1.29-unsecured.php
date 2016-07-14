fc7aa9fb10d165577b89910e41cce0f997c54e86
Emilio Pinna <emilio.pinn@gmail.com>
2016-07-14 21:46:40 +0100

    Add cli tool to debug SmartyBC

--- tests/env_php_tests/smarty-3.1.29-unsecured.php
+++ tests/env_php_tests/smarty-3.1.29-unsecured.php
@@ -2,6 +2,12 @@
 require('lib/smarty-3.1.29/libs/Smarty.class.php');
 $smarty = new SmartyBC;
 
+// Run render via CLI
+if (php_sapi_name() == "cli") {
+    $_GET["inj"] = '';
+    $_GET["tpl"] = '';
+} 
+
 $inj=$_GET["inj"];
 if(isset($_GET["tpl"])) {
   // Keep the formatting a-la-python
