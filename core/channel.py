518db57091b290ce634334e3c2d650b9cbcd470c
Emilio Pinna <emilio.pinn@gmail.com>
2016-08-25 21:10:59 +0100

    Rewrite Channel to support future multiple injection points

--- core/channel.py
+++ core/channel.py
@@ -7,30 +7,27 @@ class Channel:
     def __init__(self, args):
 
         self.args = args
-
+        
         # Consider # as part of the query string, end encode \n
         self.url = self.args.get('url').replace('#', '%23').replace('\\n', '%0A')
 
         self.base_url = self.url.split("?")[0] if '?' in self.url else self.url
 
+        self.tag = self.args.get('injection_tag')
+
         self.data = {}
 
+        self.injs = []
+        self.inj_idx = 0
+
         self.get_params = {}
-        self.get_placeholders = []
-
         self.post_params = {}
-        self.post_placeholders = []
-
         self.header_params = {}
-        self.header_placeholders = []
 
         self._parse_get()
         self._parse_post()
         self._parse_header()
         self._parse_method()
-
-        if len(self.post_placeholders + self.get_placeholders) > 1:
-            log.warn('Error, multiple placeholder in parameters')
 
     def _parse_method(self):
 
@@ -54,65 +51,128 @@ class Channel:
 
             self.header_params[param] = value
 
-            if '*' in value:
-                self.header_placeholders.append(param)
-                log.warn('Found placeholder in Header \'%s\'' % param)
+            if self.tag in param:
+                self.injs.append({
+                    'field' : 'header',
+                    'part' : 'param',
+                    'param' : param
+                })
+            if self.tag in value:
+                self.injs.append({
+                    'field' : 'header',
+                    'part' : 'value',
+                    'value': value,
+                    'param' : param
+                })
+            
                 
         # Set user agent if not set already
         user_agent = self.args.get('user_agent')
         if not user_agent:
             user_agent = 'tplmap/%s' % self.args.get('version')
             
-        if not 'User-Agent' in self.header_params:
+        if not 'user-agent' in [ p.lower() for p in self.header_params.keys() ]:
             self.header_params['User-Agent'] = user_agent
 
     def _parse_post(self):
 
         if self.args.get('data'):
 
-            datas = urlparse.parse_qs(self.args.get('data'))
+            params_dict_list = urlparse.parse_qs(self.args.get('data'))
 
-            for param_key, param_value in datas.iteritems():
+            for param, value_list in params_dict_list.items():
 
-                self.post_params[param_key] = param_value
-
-                if '*' in param_value:
-                    self.post_placeholders.append(param_key)
-                    log.warn('Found placeholder in POST parameter \'%s\'' % param_key)
-
+                self.post_params[param] = value_list
+                
+                if self.tag in param:
+                    self.injs.append({
+                        'field' : 'post',
+                        'part' : 'param',
+                        'param' : param,
+                    })
+                
+                for idx, value in enumerate(value_list):
+                    if self.tag in value:
+                        self.injs.append({
+                            'field' : 'post',
+                            'part' : 'value',
+                            'value' : value,
+                            'param' : param,
+                            'idx' : idx
+                        })  
+            
     def _parse_get(self):
 
         params_dict_list = urlparse.parse_qs(urlparse.urlsplit(self.url).query)
 
         for param, value_list in params_dict_list.items():
+
             self.get_params[param] = value_list
-
-            if any(x for x in value_list if '*' in x):
-                self.get_placeholders.append(param)
-                log.info('Found placeholder in GET parameter \'%s\'' % param)
-
-
+            
+            if self.tag in param:
+                self.injs.append({
+                    'field' : 'get',
+                    'part' : 'param',
+                    'param': param
+                })
+            
+            for idx, value in enumerate(value_list):
+                if self.tag in value:
+                    self.injs.append({
+                        'field' : 'get',
+                        'part': 'value',
+                        'param': param,
+                        'value' : value,
+                        'idx' : idx
+                    })                
+            
     def req(self, injection):
 
-        # Inject
         get_params = self.get_params.copy()
-        if self.get_placeholders:
-            get_placeholder = self.get_placeholders[0]
-            get_params[get_placeholder] = injection
-
         post_params = self.post_params.copy()
-        if self.post_placeholders:
-            post_placeholder = self.post_placeholders[0]
-            post_params[post_placeholder] = injection
-
         header_params = self.header_params.copy()
-        if self.header_placeholders:
+        
+        # Pick current injection by index
+        inj = self.injs[self.inj_idx]
+        
+        if inj['field'] == 'post':
+        
+            if inj.get('part') == 'param':
+                # If injection replaces param, save the value 
+                # with a new param
+                old_value = post_params[inj.get('param')]
+                del post_params[inj.get('param')]
+                post_params[injection] = old_value
+                
+            if inj.get('part') == 'value':
+                # If injection in value, replace value by index    
+                post_params[inj.get('param')][inj.get('idx')] = injection
 
-            if '\n' in injection:
-                log.debug('Skip payload with not compatible character for headers')
-            else:
-                header_placeholder = self.header_placeholders[0]
-                header_params[header_placeholder] = injection
+        elif inj['field'] == 'get':
+                
+            if inj.get('part') == 'param':
+                # If injection replaces param, save the value 
+                # with a new param
+                old_value = get_params[inj.get('param')]
+                del get_params[inj.get('param')]
+                get_params[injection] = old_value
+                
+            if inj.get('part') == 'value':
+                # If injection in value, replace value by index    
+                get_params[inj.get('param')][inj.get('idx')] = injection
+
+        elif inj['field'] == 'header':
+                
+            if inj.get('part') == 'param':
+                # If injection replaces param, save the value 
+                # with a new param
+                old_value = get_params[inj.get('param')]
+                del header_params[inj.get('param')]
+                header_params[injection] = old_value
+                
+            if inj.get('part') == 'value':
+                # If injection in value, replace value by index    
+                header_params[inj.get('param')] = injection
 
         result = requests.request(
             method = self.http_method,
