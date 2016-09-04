616d1965b79368aacc96eafebc9efe73b2c35522
Emilio Pinna <emilio.pinn@gmail.com>
2016-09-04 14:16:08 +0100

    Add Marko test suite

---
+++ tests/test_node_marko.py
@@ -0,0 +1,54 @@
+import unittest
+import requests
+import os
+import sys
+import random
+
+sys.path.insert(1, os.path.join(sys.path[0], '..'))
+from plugins.engines.marko import Marko
+from basetest import BaseTest
+
+
+class MarkoTests(unittest.TestCase, BaseTest):
+
+    expected_data = {
+        'language': 'javascript',
+        'engine': 'marko',
+        'evaluate' : 'javascript' ,
+        'execute' : True,
+        'read' : True,
+        'write' : True,
+        'prefix' : '',
+        'suffix': '',
+        'render': '${%(code)s}',
+        'header': '${"%(header)s"}',
+        'trailer': '${"%(trailer)s"}',
+        'bind_shell' : True,
+        'reverse_shell': True
+    }
+
+    expected_data_blind = {
+        'language': 'javascript',
+        'engine': 'marko',
+        'blind': True,
+        'execute_blind' : True,
+        'write': True,
+        'prefix' : '',
+        'suffix' : '',
+        'bind_shell' : True,
+        'reverse_shell': True
+    }
+
+    url = 'http://127.0.0.1:15004/marko?inj=*&tpl=%s'
+    url_blind = 'http://127.0.0.1:15004/blind/marko?inj=*&tpl=%s'
+    plugin = Marko
+
+
+    blind_tests = [
+        (0, 0, 'AAA%sAAA', {}),
+    ]
+
+    reflection_tests = [
+        (0, 0, '%s', {}),
+        (0, 0, 'AAA%sAAA', {}),
+    ]
