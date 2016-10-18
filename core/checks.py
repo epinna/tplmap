c40d78ddcdb096e53d63a25872af009aa79d0fab
Emilio Pinna <emilio.pinn@gmail.com>
2016-10-18 10:42:00 +0300

    Polish prints

--- core/checks.py
+++ core/checks.py
@@ -150,9 +150,9 @@ def check_template_injection(channel):
         log.info(
             """Rerun tplmap providing one of the following options:\n%(execute)s%(execute_blind)s%(bind_shell)s%(reverse_shell)s%(write)s%(read)s""" % (
                 {
-                 'execute': '\n    --os-shell\t\t\tRun shell on the target\n    --os-cmd\t\t\tExecute shell commands' if channel.data.get('execute') and not channel.data.get('execute_blind') else '',
-                 'execute_blind': '\n    --os-shell\t\t\tRun shell on the target\n    --os-cmd\t\t\tExecute shell commands' if channel.data.get('execute_blind') else '',
-                 'bind_shell': '\n    --bind-shell PORT\t\tConnect to a shell bind to a target port' if channel.data.get('bind_shell') else '',
+                 'execute': '\n    --os-shell\t\t\t\tRun shell on the target\n    --os-cmd\t\t\t\tExecute shell commands' if channel.data.get('execute') and not channel.data.get('execute_blind') else '',
+                 'execute_blind': '\n    --os-shell\t\t\t\tRun shell on the target\n    --os-cmd\t\t\tExecute shell commands' if channel.data.get('execute_blind') else '',
+                 'bind_shell': '\n    --bind-shell PORT\t\t\tConnect to a shell bind to a target port' if channel.data.get('bind_shell') else '',
                  'reverse_shell': '\n    --reverse-shell HOST PORT\tSend a shell back to the attacker\'s port' if channel.data.get('reverse_shell') else '',
                  'write': '\n    --upload LOCAL REMOTE\tUpload files to the server' if channel.data.get('write') else '',
                  'read': '\n    --download REMOTE LOCAL\tDownload remote files' if channel.data.get('read') else '',                 }
