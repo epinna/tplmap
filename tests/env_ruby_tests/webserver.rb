7a1327f6b477bf08c0821aa968f817b68e01cf12
Emilio Pinna <emilio.pinn@gmail.com>
2017-03-14 21:41:50 +0000

    Add testsuite for Ruby eval

--- tests/env_ruby_tests/webserver.rb
+++ tests/env_ruby_tests/webserver.rb
@@ -6,8 +6,18 @@ Cuba.plugin Cuba::Safe
 Cuba.define do
   on get do
     on "reflect/:engine" do |engine|
+      # Keep the formatting a-la-python
       on param("inj"), param("tpl", "%s") do |inj, tpl|
-        res.write "#{engine} #{inj} #{tpl}" 
+        
+        tpl = tpl.gsub('%s', inj)
+        
+        case engine
+        when "eval"
+          res.write eval(tpl)
+        else
+          res.write "#{engine} #{inj} #{tpl}" 
+        end
+        
       end
     end
     on 'shutdown' do
