b81e8f391e0fe7a30c162a85012a6e8fdb5279c6
Emilio Pinna <emilio.pinn@gmail.com>
2017-03-26 11:02:39 +0100

    Adjust Velocity

--- plugins/engines/velocity.py
+++ plugins/engines/velocity.py
@@ -108,29 +108,3 @@ ${output}
                 { 'level': 5, 'prefix': '*#', 'suffix' : '#*' },
 
         ])
-
-
-    def rendered_detected(self):
-
-        expected_rand = str(rand.randint_n(2))
-        if expected_rand == self.execute('echo %s' % expected_rand):
-            
-            self.set('execute', True)
-            self.set('write', True)
-            self.set('read', True)
-            self.set('bind_shell', True)
-            self.set('reverse_shell', True)
-            
-            os = self.execute("""uname""")
-            if os and re.search('^[\w-]+$', os):
-                self.set('os', os)
-
-    def blind_detected(self):
-        # No blind code evaluation is possible here, only execution
-
-        # Since execution has been used to detect blind injection,
-        # let's assume execute_blind as set.
-        self.set('execute_blind', True)
-        self.set('write', True)
-        self.set('bind_shell', True)
-        self.set('reverse_shell', True)
