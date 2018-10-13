378b6f81176ec70a578bab1bd8fc8bd39ad6e365
Emilio Pinna <emilio.pinn@gmail.com>
2018-10-13 14:41:32 +0100

    Use apache container for PHP tests

--- docker-envs/Dockerfile.php
+++ docker-envs/Dockerfile.php
@@ -1,17 +1,16 @@
-FROM php:latest
+FROM php:apache
 
 RUN apt-get update && apt-get install --upgrade dnsutils python-pip -y
 RUN pip install requests PyYAML
 
-COPY  . /apps/
-WORKDIR /apps/tests/
+RUN sed -i '0,/Listen [0-9]*/s//Listen 15002/' /etc/apache2/ports.conf
 
-RUN mkdir env_php_tests/lib/ && cd env_php_tests/lib && \
+COPY  . /apps/
+COPY tests/env_php_tests/* /var/www/html/
+
+RUN mkdir /var/www/html/lib/ && cd /var/www/html/lib && \
     curl -sL 'https://github.com/smarty-php/smarty/archive/v3.1.32.tar.gz' | tar xzf - && \
     curl -sL 'https://github.com/twigphp/Twig/archive/v1.24.1.tar.gz' | tar xzf - && \
     curl -sL 'https://github.com/twigphp/Twig/archive/v1.19.0.tar.gz' | tar xzf -
 
-EXPOSE 15002
-
-CMD cd env_php_tests && php -S 0.0.0.0:15002
-
+WORKDIR /apps/tests/
