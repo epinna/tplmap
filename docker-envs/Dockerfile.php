18a75f5c0c2540e369563002b21c97daa7436785
Emilio Pinna <emilio.pinn@gmail.com>
2018-10-13 15:21:20 +0100

    Reorganize PHP Dockerfile

--- docker-envs/Dockerfile.php
+++ docker-envs/Dockerfile.php
@@ -5,12 +5,12 @@ RUN pip install requests PyYAML
 
 RUN sed -i '0,/Listen [0-9]*/s//Listen 15002/' /etc/apache2/ports.conf
 
-COPY  . /apps/
-COPY tests/env_php_tests/* /var/www/html/
-
 RUN mkdir /var/www/html/lib/ && cd /var/www/html/lib && \
     curl -sL 'https://github.com/smarty-php/smarty/archive/v3.1.32.tar.gz' | tar xzf - && \
     curl -sL 'https://github.com/twigphp/Twig/archive/v1.24.1.tar.gz' | tar xzf - && \
     curl -sL 'https://github.com/twigphp/Twig/archive/v1.19.0.tar.gz' | tar xzf -
+
+COPY  . /apps/
+COPY tests/env_php_tests/* /var/www/html/
 
 WORKDIR /apps/tests/
