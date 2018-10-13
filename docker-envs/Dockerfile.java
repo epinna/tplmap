5a5bae19b19ed2d1b25800e4ca597d528e8119e5
Emilio Pinna <emilio.pinn@gmail.com>
2018-10-13 17:48:19 +0100

    Reorganize Java Dockerfile

--- docker-envs/Dockerfile.java
+++ docker-envs/Dockerfile.java
@@ -5,12 +5,14 @@ USER root
 RUN apt-get update && apt-get install --upgrade dnsutils python-pip -y
 RUN pip install requests PyYAML
 
-COPY  . /apps/
+COPY tests/env_java_tests/spark-app/ /apps/tests/env_java_tests/spark-app/
 WORKDIR /apps/tests/
 
 # install dependencies
 RUN cd env_java_tests/spark-app/ && sed -ie 's/id "com\.github\.johnrengelman\.shadow".*//' build.gradle && \
     gradle classes
+
+COPY . /apps/
 
 EXPOSE 15003
 
