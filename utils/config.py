749807616ab1b173827913b325c5974e8f77f3d8
reversebrain <reversebrain@localhost.localdomain>
2019-08-20 10:04:40 +0200

    Added Loader argument to yaml.load()

--- utils/config.py
+++ utils/config.py
@@ -9,7 +9,7 @@ config_folder = os.path.dirname(os.path.realpath(__file__))
 # TODO: fix this
 with open(config_folder + "/../config.yml", 'r') as stream:
     try:
-        config = yaml.load(stream)
+        config = yaml.load(stream, Loader=yaml.SafeLoader)
     except yaml.YAMLError as e:
         # logger is not yet loaded, print it roughly
         print('[!][%s] %s' % ('config', e))
